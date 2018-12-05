0dbc88bfbcdcd55136254e8449e5858539e0a547
Chase <joli.chase@gmail.com>
2018-12-04 19:12:26 -0700

    last three of the ninja exercises

---
+++ ninja4/ten/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string][]string{
+		`bond_james`:      []string{`Shaken, not stirred`, `Martinis`, `Women`},
+		`moneypenny_miss`: []string{`James Bond`, `Literature`, `Computer Science`},
+		`no_dr`:           []string{`Being evil`, `Ice cream`, `Sunsets`},
+	}
+	fmt.Println(m)
+
+	m[`fleming_ian`] = []string{`steaks`, `cigars`, `espionage`}
+	delete(m, `moneypenny_miss`)
+	for r, t := range m {
+		fmt.Println("This is the record for", r)
+		for i, t2 := range t {
+			fmt.Println("\t", i, t2)
+
+		}
+	}
+}
